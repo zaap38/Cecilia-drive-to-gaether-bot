772b46ffb53795123f7c5761659a86e9e6e6cee6
Benoît ALCARAZ <benoit.alcaraz@uni.lu>
2023-07-05 14:29:00 +0200

    wip voronoi

--- main.py
+++ main.py
@@ -122,6 +122,54 @@ class Environment:
         agent.id = len(self.agentList)
         self.agentList += [agent]
 
+    def isInf(self, a, b, f):
+        return f(a) <= f(b)
+
+    def getPriority(self, q):
+        index = None
+        for i, e in enumerate(q):
+            if index is None or self.isInf(e, q[index]):
+                index = i
+        if index is None:
+            return None
+        e = q.pop(index)
+        return e, q
+
+    def processElement(self, e, queue, nodes):
+        # update the color and add neighbours to queue
+        e.color = 2
+        neighbours = self.getNeighboursByCoord(e)
+        for n in neighbours:
+            if True:
+                queue.append(n)
+
+
+    def voronoi(self):
+        # map each cel of the grid to the closest agent
+        current = None
+        queue = []
+        nodes = []
+        for y in range(self.h):
+            for x in range(self.w):
+                agent = self.agentAt(x, y)
+                if agent is not None:
+                    queue.append(PathNode(x, y, agent.id, 1))
+                else:
+                    nodes.append(PathNode(x, y))
+
+        current, queue = self.getPriority(queue)
+        while current is not None:
+            self.processElement(current)
+            current, queue = self.getPriority(queue)
+
+
+class PathNode:
+
+    def __init__(self, x, y, agent=-1, color=0) -> None:
+        self.coord = (x, y)
+        self.agent = agent
+        self.color = color
+
 
 
 environment = Environment(4, 5)
@@ -137,3 +185,4 @@ environment.nodeGrid[2][2] = Node(TypeNode.NONE, 1, {Edge.UP, Edge.LEFT})
 environment.addAgent(0, 1, Orientation.DOWN)
 
 print(environment)
+environment.voronoi()
